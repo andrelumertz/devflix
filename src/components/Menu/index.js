6f5dfbb0534afe1b4662c89905bd245423d62871
André Martins <69599938+andrelumertz@users.noreply.github.com>
2022-01-25 02:37:11 -0300

    :tada: first commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo1 from '../../assets/img/Logo1.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo1} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
